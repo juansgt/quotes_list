85245647e1bbd7bbc31d21bb44a85b7c59c66edb
Juan Santos <juan.santos@texascontrols.com.com>
2024-02-19 11:46:36 +0100

    Primer commit. Añadido un widget "Column" cargando una lita de citas dinamicamente

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  State createState() => QuoteListState();
+}
+
+class QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genious',
+    'The truth is rarely pure and never simple'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Awesome quotes',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
+  }
+}
